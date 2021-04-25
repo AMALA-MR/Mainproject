6af5afd246516e880697cfa766512fb25e81fa21
amala <amala@gmail.com>
2021-04-25 13:17:54 +0530

    create aadhar verification

--- back_end/routes/doctors.js
+++ back_end/routes/doctors.js
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken');
 const config = require('../config/db');
 const User = require('../model/user');
 const bcrypt = require('bcryptjs');
+const validator = require('aadhaar-validator')
 
 require('dotenv').config();
 const secret = process.env.JWT_KEY;
@@ -20,33 +21,37 @@ router.get('/', (req, res, next) => {
 // @route get /doctor/register
 router.all('/register', function(req, res, next) {
     const adhar_no =req.body.adhar_no;
-    let newDoctor = new User({
-        name: req.body.name,
-        age: req.body.age,
-        gender: req.body.gender,
-        adhar_no: req.body.adhar_no,
-        phone_no: req.body.phone_no,
-        hospital: req.body.hospital,
-        password: req.body.password,
-        confirm: req.body.confirm,
-        login_type: req.body.login_type
-    });
+    if(validator.isValidNumber(adhar_no)){
+        let newDoctor = new User({
+            name: req.body.name,
+            age: req.body.age,
+            gender: req.body.gender,
+            adhar_no: req.body.adhar_no,
+            phone_no: req.body.phone_no,
+            hospital: req.body.hospital,
+            password: req.body.password,
+            confirm: req.body.confirm,
+            login_type: req.body.login_type
+        });
 
-    User.getUserByAdhar(adhar_no, (err, user)=> {
-        if(err) throw err;
-        if(user){
-            return res.json({success: false, msg: 'Already have an account'});
-        }
+        User.getUserByAdhar(adhar_no, (err, user)=> {
+            if(err) throw err;
+            if(user){
+                return res.json({success: false, msg: 'Already have an account'});
+            }
 
-    User.addUser(newDoctor, (err, user) =>{
-        if(err){
-            //console.log(err)
-            res.json({success: false, msg:'Failed to register user'});   
-        }else {
-            res.json({success: true, msg:'Doctor registered, and send request to hospital for approval'}); 
-        }
-    });
-    });
+        User.addUser(newDoctor, (err, user) =>{
+            if(err){
+                //console.log(err)
+                res.json({success: false, msg:'Failed to register user'});   
+            }else {
+                res.json({success: true, msg:'Doctor registered, and send request to hospital for approval'}); 
+            }
+        });
+        });
+    }else{
+        res.json({success: false, msg:'Invalid Aadhar No'});
+    }
 });
 
 // @desc login url for normal user 
--- back_end/routes/users.js
+++ back_end/routes/users.js
@@ -5,6 +5,8 @@ const jwt = require('jsonwebtoken');
 const config = require('../config/db');
 const User = require('../model/user');
 const bcrypt = require('bcryptjs');
+const validator = require('aadhaar-validator')
+
 
 require('dotenv').config();
 const secret = process.env.JWT_KEY;
@@ -20,44 +22,48 @@ router.get('/', (req, res, next) => {
 // @route get /users/register
 router.all('/register', function(req, res, next) {
     const adhar_no =req.body.adhar_no;
-    let newUser = new User({
-        name: req.body.name,
-        age: req.body.age,
-        gender: req.body.gender,
-        adhar_no: req.body.adhar_no,
-        phone_no: req.body.phone_no,
-        password: req.body.password,
-        login_type: req.body.login_type
-    });
+    if(validator.isValidNumber(adhar_no)){
+        let newUser = new User({
+            name: req.body.name,
+            age: req.body.age,
+            gender: req.body.gender,
+            adhar_no: req.body.adhar_no,
+            phone_no: req.body.phone_no,
+            password: req.body.password,
+            login_type: req.body.login_type
+        });
 
-    User.getUserByAdhar(adhar_no, (err, user)=> {
-        if(err) throw err;
-        if(user){
-            return res.json({success: false, msg: 'Already have an account'});
-        }
+        User.getUserByAdhar(adhar_no, (err, user)=> {
+            if(err) throw err;
+            if(user){
+                return res.json({success: false, msg: 'Already have an account'});
+            }
 
-    User.addUser(newUser, (err, user) =>{
-        if(err){
-            console.log(err)
-            res.json({success: false, msg:'Failed to register user'});   
-        }else {
-            const token = jwt.sign(user.toJSON(), process.env.JWT_KEY,{
-                expiresIn: 604800 // one week
-            });
-            res.json({
-                success: true, token: 'JWT '+token,
-                user:{
-                    id: user._id,
-                    name: user.name,
-                    age: user.age,
-                    gender: user.gender,
-                    login_type: user.login_type,
-                }
-            });
-            //res.json({success: true, msg:'User registered'}); 
-        }
-    });
-    });
+        User.addUser(newUser, (err, user) =>{
+            if(err){
+                console.log(err)
+                res.json({success: false, msg:'Failed to register user'});   
+            }else {
+                const token = jwt.sign(user.toJSON(), process.env.JWT_KEY,{
+                    expiresIn: 604800 // one week
+                });
+                res.json({
+                    success: true, token: 'JWT '+token,
+                    user:{
+                        id: user._id,
+                        name: user.name,
+                        age: user.age,
+                        gender: user.gender,
+                        login_type: user.login_type,
+                    }
+                });
+                //res.json({success: true, msg:'User registered'}); 
+            }
+        });
+        });
+    }else{
+        res.json({success: false, msg:'Invalid Aadhar No'});
+    }
 });
 
 
