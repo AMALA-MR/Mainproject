b2f6b5b3cd42ee6ccb73b538d6d5e2c49f5330a7
jishnu <jishnukrishnan99@gmail.com>
2021-05-06 10:51:09 +0530

    booking added

--- back_end/model/booking.js
+++ back_end/model/booking.js
@@ -16,14 +16,34 @@ const BookingSchema = mongoose.Schema({
         type: Date,
         required: true
     },
-    time:{
+    slot:{
         type: String,
-        required: true
+        required: true,
+        enum:['10am-11am','11am-12pm','12pm-01pm','02pm-03pm','03pm-04pm']
     },
     status: {
         type: String,
-        required: true
+        required: true,
+        default: 'book',
+        enum:['book','complete','cancel'],
     }
 });
 
 const Booking = module.exports = mongoose.model('Booking', BookingSchema);
+
+//book time slot and date
+module.exports.addBook = function(newBook, callback){
+    newBook.save(callback)
+}
+
+//check date and time avaliable
+module.exports.checkTimeSlot = function(date,slot,hospital,callback){
+    const query = {date: date, slot: slot, hospital: hospital}
+    Booking.find(query,callback).populate('hospital')
+}
+
+//view avaliable time slot
+module.exports.avaliableSlot = function(date,hospital,callback){
+    const query = {date: date, hospital: hospital}
+    Booking.find(query,callback).populate('hospital')
+}
